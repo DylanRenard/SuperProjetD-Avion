e59fc1ffd949f1df1ac65d68026b959a597ba863
Unknown <loredana.bernard.etu@univ-lemans.fr>
2018-11-18 19:03:43 +0100

    fonction decode (en cours)

--- SuperProjetAvion/app/src/main/java/fr/ensim/superprojetavion/Model/CodeInfo.java
+++ SuperProjetAvion/app/src/main/java/fr/ensim/superprojetavion/Model/CodeInfo.java
@@ -53,15 +53,114 @@ public class CodeInfo {
 
 
     void onDecode() {
+        // A)
         decode_location = airport.getAirportName();
 
+        // B)
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMddHHmm", Locale.getDefault());
         try {
             Date date = dateFormat.parse(code_date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        decode_date = code_date.toString();
 
+        // C)
+        // TO DO
+
+        // D)
+        decode_clearedRunwayLength = " CLEARED RUNWAY LENGTH " + code_clearedRunwayLength + "M";
+
+        // E)
+        String axis="";
+        String [] clearedRunwayWidth = code_clearedRunwayWidth.split("");
+
+        if (clearedRunwayWidth[clearedRunwayWidth.length -1].equals("L")){
+            axis = "LEFT";
+        }
+        else {
+            axis = "RIGHT";
+        }
+
+        if (clearedRunwayWidth.length == 2) {
+            decode_clearedRunwayWidth = "CLEARED RUNWAY WIDTH " + clearedRunwayWidth[0] + "M " + axis;
+        }
+        else {
+            decode_clearedRunwayWidth = "CLEARED RUNWAY WIDTH " + clearedRunwayWidth[0] + clearedRunwayWidth[1] + "M " + axis;
+        }
+
+        // F)
+        String [] conditionsRunway = code_conditionsRunway.split("/");
+        decode_conditionsRunway = "Threshold: " + switchConditions(conditionsRunway[0]) + " / "
+                                 + "Mid runway: " + switchConditions(conditionsRunway[1]) + " / "
+                                 + "Roll out: " + switchConditions(conditionsRunway[2]) ;
+
+        // G)
+        String[] thickness = code_thickness.split("/");
+        decode_thickness = "MEAN DEPTH Threshold: " + thickness[0] + "mm / "
+                            + "Mid runway: " + thickness[1] + "mm / "
+                            + "Roll out: " + thickness[2] + "mm";
+
+        // H)
+        
+        // J)
+
+        // K)
+
+        // L)
+
+        // M)
+
+        // N)
+
+        // P)
+
+
+        // R)
+
+        // S)
+
+
+        // T)
+
+
+    }
+
+    String switchConditions(String number){
+        String value="";
+        switch (number.toLowerCase()){
+            case "0":
+                value = "CLEAR AND DRY";
+                break;
+            case "1":
+                value = "DAMP";
+                break;
+            case "2":
+                value = "WET or WATER PATCHES";
+                break;
+            case "3":
+                value = "RIME OR FROST COVERED";
+                break;
+            case "4":
+                value = "DRY SNOW";
+                break;
+            case "5":
+                value = "WET SNOW";
+                break;
+            case "6":
+                value = "SLUSH";
+                break;
+            case "7":
+                value = "ICE";
+                break;
+            case "8":
+                value = "COMPACTED OR ROLLED SNOW";
+                break;
+            case "9":
+                value = "FROZEN RUTS OR RIDGES";
+                break;
+        }
+        return value;
     }
 
 
