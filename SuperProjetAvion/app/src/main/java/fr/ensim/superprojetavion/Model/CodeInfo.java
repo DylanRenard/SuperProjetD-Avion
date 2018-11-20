2cb88b77c5600d93ef86b7d4dde02d9f64f52fc5
Unknown <loredana.bernard.etu@univ-lemans.fr>
2018-11-20 09:21:15 +0100

    fonction decode (en cours2)

--- SuperProjetAvion/app/src/main/java/fr/ensim/superprojetavion/Model/CodeInfo.java
+++ SuperProjetAvion/app/src/main/java/fr/ensim/superprojetavion/Model/CodeInfo.java
@@ -56,6 +56,7 @@ public class CodeInfo {
         // A)
         decode_location = airport.getAirportName();
 
+
         // B)
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMddHHmm", Locale.getDefault());
         try {
@@ -65,11 +66,13 @@ public class CodeInfo {
         }
         decode_date = code_date.toString();
 
+
         // C)
         // TO DO
 
         // D)
         decode_clearedRunwayLength = " CLEARED RUNWAY LENGTH " + code_clearedRunwayLength + "M";
+
 
         // E)
         String axis="";
@@ -89,11 +92,13 @@ public class CodeInfo {
             decode_clearedRunwayWidth = "CLEARED RUNWAY WIDTH " + clearedRunwayWidth[0] + clearedRunwayWidth[1] + "M " + axis;
         }
 
+
         // F)
         String [] conditionsRunway = code_conditionsRunway.split("/");
         decode_conditionsRunway = "Threshold: " + switchConditions(conditionsRunway[0]) + " / "
                                  + "Mid runway: " + switchConditions(conditionsRunway[1]) + " / "
                                  + "Roll out: " + switchConditions(conditionsRunway[2]) ;
+
 
         // G)
         String[] thickness = code_thickness.split("/");
@@ -101,9 +106,41 @@ public class CodeInfo {
                             + "Mid runway: " + thickness[1] + "mm / "
                             + "Roll out: " + thickness[2] + "mm";
 
+
         // H)
-        
+        String[] frictionCoefInstru = code_frictionCoef.split(" ");  // Separates coef and instrument
+        String[] frictionCoef = frictionCoefInstru[0].split("/");    // Separates all coef
+        String[] frictionCoefValue={"","",""};                            // Contains values of coef
+        if(frictionCoef[0].length() == 1){
+            frictionCoefValue = switchEstimatedCoef(frictionCoef);
+        }
+        else{
+            frictionCoefValue = switchCalculatedCoef(frictionCoef);
+        }
+        decode_frictionCoef = "BRAKING ACTION Threshold: " + frictionCoefValue[0] +  " / " +
+                                "Mid runway: " + frictionCoefValue[1] + " / " +
+                                "Roll out: " + frictionCoefValue[2] + " " +
+                                "Instrument: " + switchInstrument(frictionCoefInstru[1]);
+
+
         // J)
+        String[] criticalSnowbanks = code_criticalSnowbanks.split("/");
+        String directionDistance = criticalSnowbanks[1];
+        String distance = "";
+        String direction ="";
+        if(directionDistance.length() == 2 ){
+            distance = directionDistance.substring(0,1);
+            direction =directionDistance.substring(2);
+        }
+        if (directionDistance.length() == 3){
+            if (directionDistance.substring(2,3).equals("L")){
+                
+            }
+        }
+
+        decode_criticalSnowbanks = "CRITICAL SNOW BANK " + criticalSnowbanks[0] + "cm / " +
+                                    distance + "m" + direction + " of Runway";
+
 
         // K)
 
@@ -126,6 +163,7 @@ public class CodeInfo {
 
     }
 
+    // F) FONCTIONS
     String switchConditions(String number){
         String value="";
         switch (number.toLowerCase()){
@@ -161,6 +199,94 @@ public class CodeInfo {
                 break;
         }
         return value;
+    }
+
+
+    // H) FONCTIONS
+    String[] switchEstimatedCoef(String[] chaine){
+        String [] value ={"","",""};
+        for (int i=0 ; i< chaine.length ; i++){
+            String number = chaine[i];
+            switch (number.toLowerCase()){
+                case "5":
+                    value[i] ="GOOD";
+                    break;
+                case "4":
+                    value[i]="MEDIUM TO GOOD";
+                    break;
+                case "3":
+                    value[i]="MEDIUM";
+                    break;
+                case "2":
+                    value[i]="MEDIUM TO POOR";
+                    break;
+                case "1":
+                    value[i]="POOR";
+                    break;
+            }
+        }
+
+        return value;
+    }
+
+    String [] switchCalculatedCoef (String[] chaine){
+        String [] value ={"","",""};
+        for (int i=0 ; i< chaine.length ; i++){
+            String number = chaine[i];
+            if (Integer.parseInt(number) >= 40){
+                value[i] = "GOOD";
+            }
+            else if(Integer.parseInt(number) <40 && Integer.parseInt(number) >= 36 ) {
+                value[i] = "MEDIUM TO GOOD";
+            }
+            else if(Integer.parseInt(number) <36 && Integer.parseInt(number) >= 30 ) {
+                value[i] = "MEDIUM";
+            }
+            else if(Integer.parseInt(number) <30 && Integer.parseInt(number) >= 26 ) {
+                value[i] = "MEDIUM TO POOR";
+            }
+            else if(Integer.parseInt(number) <26) {
+                value[i] = "POOR";
+            }
+        }
+        return value;
+    }
+
+    String switchInstrument(String letters){
+        String instrument ="";
+        switch (letters.toLowerCase()){
+            case "BRD":
+                instrument="Brakemeter-Dynometer";
+                break;
+            case "GRT":
+                instrument="Grip tester";
+                break;
+            case "MUM":
+                instrument="Mu-meter";
+                break;
+            case "RFT":
+                instrument ="Runway friction tester";
+                break;
+            case "SFH":
+                instrument="Surface friction tester (high-pressure tire)";
+                break;
+            case "SFL":
+                instrument="Surface friction tester (low-pressure tire)";
+                break;
+            case "SKH":
+                instrument="Skiddometer (high-pressure tire)";
+                break;
+            case "SKL":
+                instrument="Skiddometer (low-pressure tire)";
+                break;
+            case "TAP":
+                instrument="Tapley meter";
+                break;
+            case "":
+                instrument="";
+                break;
+        }
+        return  instrument;
     }
 
 
